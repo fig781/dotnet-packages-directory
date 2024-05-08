cf57d38d8d5df5f0f21b05ceb2b2a3d3d86806bd
Aden Eilers <adeneilers@gmail.com>
2024-05-07 21:24:10 -0700

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://frameworkdotnet.com',
-  integrations: [mdx(), sitemap(), react()]
+  integrations: [mdx(), sitemap(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })]
 });
