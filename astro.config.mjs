7eef306142efc45d2b590e24a6c9ceb8eafbd064
Aden Eilers <adeneilers@gmail.com>
2024-05-07 21:30:34 -0700

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 
 // https://astro.build/config
